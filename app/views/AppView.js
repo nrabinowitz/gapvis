24297fdce41a41634b3fc2af70afc6cddbde814d
nrabinowitz <public@nickrabinowitz.com>
2012-05-14 11:15:23 -0700

    Cleanup, removing jquery UI dependencies

--- app/views/AppView.js
+++ app/views/AppView.js
@@ -6,21 +6,26 @@
     // CSS-based slide
     function slide($el, slideIn, direction) {
         if ($.support.transition) {
+        
             var slideClass = 'sliding',
                 startPos = !slideIn ? 'active' : 
                     direction == 'left' ? 'next' : 'prev';
-            console.log('starting slide ' + (slideIn ? 'in' : 'out') + ' ' + direction);
+                    
+            // set initial position
             $el.addClass(startPos)
                 .show();
+                
+            // force reflow
             $el[0].offsetWidth;
             
+            // add transition classes
             $el.addClass([slideClass, direction].join(' '))
                 // listen for event end
                 .one($.support.transition.end, function () {
-                    console.log('ending slide ' + (slideIn ? 'in' : 'out') + ' ' + direction);
                     if (!slideIn) $el.hide();
                     $el.removeClass([startPos, direction, slideClass].join(' '));
                 });
+                
         } else $el.toggle(slideIn);
     }
     
