aca3f7380d092025a52b980871b3582c3460cfea
nrabinowitz <public@nickrabinowitz.com>
2012-05-14 13:44:02 -0700

    Tweaking styles, using bootstrap buttons

--- app/views/NavigationView.js
+++ app/views/NavigationView.js
@@ -1,5 +1,5 @@
 /*
- * Permalink View
+ * Book Navigation View
  */
 define(['gv', 'views/BookView'], function(gv, BookView) {
     var state = gv.state;
@@ -7,12 +7,6 @@ define(['gv', 'views/BookView'], function(gv, BookView) {
     // View: NavigationView
     return BookView.extend({
         template: '#navigation-view-template',
-        
-        navViews: [
-            'book-summary',
-            'reading-view', 
-            'place-view'
-        ],
     
         initialize: function() {
             var view = this;
@@ -23,21 +17,11 @@ define(['gv', 'views/BookView'], function(gv, BookView) {
         },
         
         render: function() {
-            var view = this,
-                cid = view.cid;
+            var view = this;
             // render content and append to parent
-            view.renderTemplate({ cid: cid });
+            view.renderTemplate();
             // button it
-            view.$('.nav-buttons').buttonset();
-            $('#book-summary-' + cid).button('option', 'icons', {primary:'ui-icon-star'});
-            $('#reading-view' + cid).button('option', 'icons', {primary:'ui-icon-note'});
-            $('#place-view' + cid).button('option', 'icons', {primary:'ui-icon-pin-s'});
-            view.$('.permalink').button({
-                icons: {
-                    primary: "ui-icon-link"
-                },
-                text: false
-            });
+            view.$('.btn-group').button();
             // update
             view.updatePermalink();
             view.updateNavButtons();
@@ -45,20 +29,17 @@ define(['gv', 'views/BookView'], function(gv, BookView) {
         },
         
         updatePermalink: function() {
-            $('a.permalink').attr('href', gv.router.getPermalink());
+            this.$('a.permalink').attr('href', gv.router.getPermalink());
         },
         
         updateNavButtons: function() {
             // enable/disable place view
-            $('#place-view-' + this.cid)
-                .button(state.get('placeid') ? 'enable' : 'disable');
+            this.$('[data-view-id=place-view]')
+                .attr('disabled', state.get('placeid') ? '' : 'disabled');
             // check the appropriate button
-            var viewKey = state.get('view'),
-                cid = this.cid;
-            this.navViews.forEach(function(nav) {
-                $('#' + nav + '-' + cid)
-                    .prop('checked', nav == viewKey)
-                    .button('refresh');
+            this.$('button').each(function() {
+                var $this = $(this);
+                $this.toggleClass('active', $this.attr('data-view-id') == state.get('view'));
             });
         },
         
