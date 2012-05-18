f8453dcb48fb5717f45dd1d9dcb0a79ee0e0ab09
nrabinowitz <public@nickrabinowitz.com>
2012-05-18 12:32:19 -0700

    Fixing map initialization problem

--- app/views/BookSummaryMapView.js
+++ app/views/BookSummaryMapView.js
@@ -13,54 +13,59 @@ define(['gv', 'views/BookView'], function(gv, BookView) {
         
         render: function() {
             if (DEBUG && !window.google) return;
-            var book = this.model,
-                markers = this.markers = [],
+            var view = this,
+                book = view.model,
+                markers = view.markers = [],
                 gmaps = google.maps,
                 colorScale = d3.scale.quantize()
                     .domain([1, book.places.first().get('frequency')])
                     .range(colorThemes),
                 bounds = book.gmapBounds(),
-                gmap = new gmaps.Map(this.el, {
-                    center: bounds.getCenter(),
-                    zoom: 4,
-                    mapTypeId: gmaps.MapTypeId.TERRAIN,
-                    streetViewControl: false,
-                    zoomControlOptions: {
-                        style: google.maps.ZoomControlStyle.LARGE
-                    },
-                    styles: mapStyle
-                });
+                $container = $('<div></div>').appendTo(view.el);
                 
-            // set bounds
-            gmap.fitBounds(bounds);
-            
-            book.places.each(function(place) {
-                var theme = colorScale(place.get('frequency')),
-                    w = 10,
-                    c = w/2,
-                    icon = TimeMapTheme.getCircleUrl(w, theme.color, '99');
-                    size = new gmaps.Size(w, w),
-                    anchor = new gmaps.Point(c, c),
-                    marker = new gmaps.Marker({
-                        icon: new gmaps.MarkerImage(
-                            icon,
-                            size,
-                            new gmaps.Point(0,0),
-                            anchor,
-                            size
-                        ),
-                        position: place.gmapLatLng(), 
-                        map: gmap, 
-                        title: place.get('title')
+            // deal with layout issues - div must be visible in DOM before map initialization
+            setTimeout(function() {
+                var gmap = new gmaps.Map($container[0], {
+                        center: bounds.getCenter(),
+                        zoom: 4,
+                        mapTypeId: gmaps.MapTypeId.TERRAIN,
+                        streetViewControl: false,
+                        zoomControlOptions: {
+                            style: google.maps.ZoomControlStyle.LARGE
+                        },
+                        styles: mapStyle
                     });
-                // UI listener
-                gmaps.event.addListener(marker, 'click', function() {
-                    state.set({ placeid: place.id });
-                    state.set({ view: 'place-view' });
+                    
+                // set bounds
+                gmap.fitBounds(bounds);
+                
+                book.places.each(function(place) {
+                    var theme = colorScale(place.get('frequency')),
+                        w = 10,
+                        c = w/2,
+                        icon = TimeMapTheme.getCircleUrl(w, theme.color, '99');
+                        size = new gmaps.Size(w, w),
+                        anchor = new gmaps.Point(c, c),
+                        marker = new gmaps.Marker({
+                            icon: new gmaps.MarkerImage(
+                                icon,
+                                size,
+                                new gmaps.Point(0,0),
+                                anchor,
+                                size
+                            ),
+                            position: place.gmapLatLng(), 
+                            map: gmap, 
+                            title: place.get('title')
+                        });
+                    // UI listener
+                    gmaps.event.addListener(marker, 'click', function() {
+                        state.set({ placeid: place.id });
+                        state.set({ view: 'place-view' });
+                    });
+                    markers.push(marker);
                 });
-                markers.push(marker);
-            });
-            
+            }, 0);
         }
     });
     
