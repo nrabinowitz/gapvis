d2c6fa85b05407b480e087e9fe1cf094574d5a97
nrabinowitz <public@nickrabinowitz.com>
2012-05-21 13:17:31 -0700

    Adding file for slide utility

---
+++ app/util/slide.js
@@ -0,0 +1,32 @@
+/*
+ * Utility: CSS-based slide function
+ */
+define(function() {
+    
+    // CSS-based slide
+    return function slide($el, slideIn, direction) {
+        if ($.support.transition) {
+        
+            var slideClass = 'sliding',
+                startPos = !slideIn ? 'active' : 
+                    direction == 'left' ? 'next' : 'prev';
+                    
+            // set initial position
+            $el.addClass(startPos)
+                .show();
+                
+            // force reflow
+            $el[0].offsetWidth;
+            
+            // add transition classes
+            $el.addClass([slideClass, direction].join(' '))
+                // listen for event end
+                .one($.support.transition.end, function () {
+                    if (!slideIn) $el.hide();
+                    $el.removeClass([startPos, direction, slideClass].join(' '));
+                });
+                
+        } else $el.toggle(slideIn);
+    }
+    
+});
