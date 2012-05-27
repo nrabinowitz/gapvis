959b484ddab667e77bc32e7c94a4c80db40a6b20
nrabinowitz <public@nickrabinowitz.com>
2012-05-26 20:40:50 -0700

    Adding compressed shim for production build

---
+++ app/util/shim-min.js
@@ -0,0 +1,2 @@
+// let d3 fail gracefully in older IE versions
+(function(a){if(!a.CSSStyleDeclaration){var b=a.CSSStyleDeclaration=function(){};b.prototype.setProperty=function(){},a.nod3=!0}})(this);
